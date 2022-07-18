368a6dee6dc3e86d4a1c859e7e21e8b29c9d4a93
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-07-18 10:35:52 +0100

    added padding to the nav

--- src/pages/error.js
+++ src/pages/error.js
@@ -7,7 +7,9 @@ const Error = () => {
   }, []);
   return (
     <div className='min-h-[80vh] flex flex-col justify-center items-center'>
-      <h4 className='font-bold text-5xl'>404 | Page Not Found</h4>
+      <h4 className='font-bold text-3xl sm:text-5xl text-center'>
+        404 | Page Not Found
+      </h4>
       <Link to='/'>
         <button className='py-3 px-6 my-4 rounded-lg bg-[#7805A7] text-sm text-white  w-fit'>
           Back Home
